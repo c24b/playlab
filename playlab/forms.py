b2c01da976b84f4f94b131aefdf4b87097808e1f
Constance de Quatrebarbes <4barbes@gmail.com>
2015-10-01 16:37:42 +0200

    Pas compris l'histoire du redirect

--- playlab/forms.py
+++ playlab/forms.py
@@ -1,1 +1,14 @@
+from django import forms
+#from .models import *
+
+class UserProfileForm(forms.Form):
+    def __init__(self,req="request",instance={}):
+        self.req = req
+        self.instance = instance
+    
+    first_name = forms.CharField(label='First name', max_length=30)
+    last_name = forms.CharField(label='First name', max_length=30)
+    username = forms.CharField(label='First name', max_length=30)
+    email = forms.EmailField(label='First name', max_length=30)
+    
 
